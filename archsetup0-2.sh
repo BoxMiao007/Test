a383e376cfb7d118506455abd7d2344ea84e0405
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2024-04-21 20:18:03 +0800

    Update archsetup0-2.sh

--- archsetup0-2.sh
+++ archsetup0-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 使用 pacstrap 在 /mnt 目录下初始化密钥环并安装软件包
-pacstrap -K /mnt base base-devel linux-firmware networkmanager vim nano git zsh reflector amd-ucode noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
+pacstrap -K /mnt base base-devel linux-firmware networkmanager ntfs-3g vim nano git zsh reflector amd-ucode noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
 
 # 启用必要的服务
 sudo systemctl enable --now NetworkManager 
