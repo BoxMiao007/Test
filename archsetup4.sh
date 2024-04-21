a6bdbe347afeb6cfafc62e003911104809ff8725
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2024-04-21 12:40:13 +0800

    Update and rename archsetup3.sh to archsetup4.sh

---
+++ archsetup4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 获取最新的镜像列表
+reflector -c China -l 10 --sort rate --save /etc/pacman.d/mirrorlist
+
+# 编辑 pacman 配置文件
+sed -i 's/^#Color/Color/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+
+# 更新系统密钥环
+pacman -Sy archlinux-keyring --noconfirm
+
+# 启用必要的服务
+sudo systemctl enable NetworkManager 
+sudo systemctl enable bluetooth
+
+# 更新 archinstall 工具
+sudo pacman -Sy archinstall --noconfirm
+
+echo "success"
