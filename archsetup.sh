5d60f0690d4c8e9aa1a306d167767383f8ff9042
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2024-04-21 11:31:25 +0800

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -10,8 +10,14 @@ sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 # 更新系统密钥环
 pacman -Sy archlinux-keyring --noconfirm
 
+# 确保 /mnt 是一个挂载点
+if ! mountpoint -q /mnt; then
+    echo "/mnt is not a mountpoint. Please mount your target system root directory to /mnt before running this script."
+    exit 1
+fi
+
 # 使用 pacstrap 在 /mnt 目录下初始化密钥环并安装软件包
-pacstrap -K /mnt base base-devel dhcpcd linux-firmware networkmanager systemd-networkd systemd-resolved bluez bluez-utils vim nano git zsh openssh ntfs-3g reflector amd-ucode wireplumber alsa-utils man-db man-pages texinfo noto-fonts noto-fonts-cjk noto-fonts-emoji gtkmm3 --noconfirm
+pacstrap -K /mnt base base-devel dhcpcd linux-firmware networkmanager systemd-networkd systemd-resolvconf bluez bluez-utils vim nano git zsh openssh ntfs-3g reflector amd-ucode wireplumber alsa-utils man-db man-pages texinfo noto-fonts noto-fonts-cjk noto-fonts-emoji gtkmm3 --noconfirm
 
 # 启用必要的服务
 arch-chroot /mnt systemctl enable NetworkManager 
