e3566c9aea179eccba90b83f180c002fd1c8a593
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2024-04-21 12:49:38 +0800

    Update and rename archsetup0-1.sh to archsetup0-2.sh

--- archsetup0-2.sh
+++ archsetup0-2.sh
@@ -3,5 +3,11 @@
 # 使用 pacstrap 在 /mnt 目录下初始化密钥环并安装软件包
 pacstrap -K /mnt base base-devel linux-firmware networkmanager systemd-networkd systemd-resolvconf bluez bluez-utils vim nano git zsh openssh ntfs-3g reflector amd-ucode wireplumber alsa-utils man-db man-pages texinfo noto-fonts noto-fonts-cjk noto-fonts-emoji gtkmm3 --noconfirm
 
+# 启用必要的服务
+sudo systemctl enable NetworkManager 
+sudo systemctl enable bluetooth
+sudo systemctl enable systemd-networkd
+sudo systemctl enable systemd-resolvconf
+
 echo "success"
 
