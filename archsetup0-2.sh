4eeb6b2333ffddef7203d610827ee8070e84a292
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2024-04-21 15:35:54 +0800

    Update archsetup0-2.sh

--- archsetup0-2.sh
+++ archsetup0-2.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # 使用 pacstrap 在 /mnt 目录下初始化密钥环并安装软件包
-pacstrap -K /mnt base base-devel linux-firmware networkmanager systemd-networkd systemd-resolvconf bluez bluez-utils vim nano git zsh openssh ntfs-3g reflector amd-ucode wireplumber alsa-utils man-db man-pages texinfo noto-fonts noto-fonts-cjk noto-fonts-emoji gtkmm3 --noconfirm
+pacstrap -K /mnt base base-devel linux-firmware networkmanager vim nano git zsh reflector amd-ucode noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
 
 # 启用必要的服务
-sudo systemctl enable NetworkManager 
-sudo systemctl enable bluetooth
-sudo systemctl enable systemd-networkd
-sudo systemctl enable systemd-resolvconf
-
+sudo systemctl enable --now NetworkManager 
 echo "success"
 
